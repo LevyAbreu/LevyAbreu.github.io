40edc75ced03e6e75cf669add0120cd3db20ce06
victor.levy.trabs <victor.levy.trabs@gmail.com>
2025-07-17 03:06:42 +0000

    commit 1.1

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-// Add JS here
+function scrollToSlide(index) {
+    const slideWidth = window.innerWidth;
+    const wrapper = document.getElementById("slides");
+    wrapper.style.transform = `translateX(-${index * slideWidth}px)`;
+    setActiveMenu(index);
+    sessionStorage.setItem("currentSlide", index);
+}
+  
+window.onload = () => {
+    const savedSlide = sessionStorage.getItem("currentSlide") || 0;
+    scrollToSlide(savedSlide);
+};
+
+function setActiveMenu(index) {
+    document.querySelectorAll("nav a").forEach((a, i) => {
+      if (i === index) a.classList.add("active");
+      else a.classList.remove("active");
+    });
+}
